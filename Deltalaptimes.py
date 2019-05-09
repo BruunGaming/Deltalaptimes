4a50f41d35f58529c2c3b037f8647e89f443f5a8
BruunGaming <33825901+BruunGaming@users.noreply.github.com>
2019-05-09 11:02:52 +0200

    added text to input
    now you can see which to type when

--- Deltalaptimes.py
+++ Deltalaptimes.py
@@ -6,16 +6,15 @@ datetimeFormat = '%M:%S.%f'
 #time1= '1:22.000'
 #time2= '2:23.111'
 
-time1 = input()
-time2 = input()
+time1 = input("Type 1st lap ")
+time2 = input("Type 2nd lap ")
 
 diff = datetime.datetime.strptime(time2, datetimeFormat)\
 	 - datetime.datetime.strptime(time1, datetimeFormat)
 
 #Interval= datetime.datetime.strptime(diff, datetimeFormat)
 
-print(diff.__format__
-	  datetimeFormat)
+print(diff)
 
 
 
