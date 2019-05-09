eb8d2053ea9cf0a50ca19b38800e01ad3b0b523e
BruunGaming <33825901+BruunGaming@users.noreply.github.com>
2019-05-09 11:00:20 +0200

    Added Input from user
    user is now able to type the times into the command line

--- Deltalaptimes.py
+++ Deltalaptimes.py
@@ -1,19 +1,46 @@
 import datetime
-from datetime import timedelta
+from time import strftime
 
 datetimeFormat = '%M:%S.%f' 
 
-time1 = '1:22.000'
-time2 = '2:23.111'
+#time1= '1:22.000'
+#time2= '2:23.111'
 
-
-
+time1 = input()
+time2 = input()
 
 diff = datetime.datetime.strptime(time2, datetimeFormat)\
 	 - datetime.datetime.strptime(time1, datetimeFormat)
 
+#Interval= datetime.datetime.strptime(diff, datetimeFormat)
+
+print(diff.__format__
+	  datetimeFormat)
+
+
+
+
+
+
+
+
+""" det som næsten virkede
+import datetime, time
+from time import strftime
+
+datetimeFormat = '%M:%S.%f' 
+
+diff = datetime.datetime.strptime(time2, datetimeFormat)\
+ - datetime.datetime.strptime(time1, datetimeFormat)
 
 print(diff)
 
+"""
 
 
+""" Ting jeg har prøvet
+
+
+#FormatTime = tistrtime(diff, '%M:%S.%f')
+#print ("%M:%S.%f")diff
+"""
